57f1a642a4f776354717d92975ecc9d8986e3f3d
basit <ultimatewarriorx3@gmail.com>
2022-09-08 23:09:43 +0530

    C

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ let port=process.env.PORT||3000
 app.get("/",(req,res,next)=>
 {
     res.write("Welcome\n'/get-weather/<district-name>' to get weather\n'/add-district/<district-name>/<district-code>'  to add a new district\n'/district-list' To list all districts available")
-    
+    res.write("\n\nNote:\n\tTemperatute in deg C\n\tVisibility in km\n\tPressure in mbar\n\tDew Point in deg C\n")
+    res.write("\n\nAll weather data is taken from https://www.timeanddate.com/")
     res.end()
     next()
 })
