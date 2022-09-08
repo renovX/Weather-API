e4871427f0fcd774f61843a780cffe218ab78ac6
basit <ultimatewarriorx3@gmail.com>
2022-09-08 22:39:18 +0530

    commit2

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app=express()
 let port=process.env.PORT||3000
 app.get("/",(req,res,next)=>
 {
-    res.write("Welcome\n'/getweather/<district-name>' to get weather\nadd-district/<district-name>/<district-code>'  to add a new district\n'/district-list' To list all districts available")
+    res.write("Welcome\n'/get-weather/<district-name>' to get weather\nadd-district/<district-name>/<district-code>'  to add a new district\n'/district-list' To list all districts available")
     res.write("'/get-weather/<district-name>' to get weather\n")
     res.write("'/add-district/<district-name>/<district-code>'  to add a new district\n")
     res.write("'/district-list' To list all districts available")
